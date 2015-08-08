5dbefc5ba340e547ddc937dce90e09b8a326e20f
Charles Milam <charles.milam@gmail.com>
2015-08-08 14:16:36 -0500

    Modify default test scaffolding to meet current app settings. Fix broken test on list length.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,9 +10,7 @@
 angular.module('yoTutApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3'
+  
     ];
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
